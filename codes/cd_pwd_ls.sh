fa7fe19e25c9d44b6fd2d0470252a0a7794f88b9
seunghoshin359 <orcb123@naver.com>
2025-07-14 18:07:10 +0900

    Create cd_pwd_ls.sh

---
+++ codes/cd_pwd_ls.sh
@@ -0,0 +1,10 @@
+[fedora@localhost Downloads]$ cd /home/fedora/Music
+[fedora@localhost Music]$ pwd
+/home/fedora/Music
+[fedora@localhost Music]$ ls
+[fedora@localhost Music]$ cd /home/fedora/Pictures
+[fedora@localhost Pictures]$ pwd
+/home/fedora/Pictures
+[fedora@localhost Pictures]$ ls
+[fedora@localhost Pictures]$ 
+##오늘 pwd 와 cd 와 ls 를 배웠습니다.
